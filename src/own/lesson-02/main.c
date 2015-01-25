d9106af8240bd73e16d1de49463d997a1453c20b
Nino Ninux <nino.ninux@gmail.com>
2015-01-25 13:56:45 +0100

    created an example based on lesson 2

--- src/own/lesson-02/main.c
+++ src/own/lesson-02/main.c
@@ -8,24 +8,41 @@
 #include "settings.h"
 #include "sdladdon.h"
 
-SDL_Window *window;
-SDL_Surface *window_surface;
-SDL_Surface *image;
+SDL_Window *window = NULL;
+SDL_Surface *surface = NULL;
+SDL_Surface *image = NULL;
 char bitmap[] = "hello_world.bmp";
+char title[] = "bitmaps";
 
 int main(char argc, char **argv)
 {
-	if (!init(window, window_surface, "loading a bitmap")) {
+	if (!init(&window, &surface, title)) {
 		printf("Failed to initialize!\n");
-	} else {
-		if (!load_bitmap(image, bitmap)) {
-			printf("Failed to load the image!\n");
-		} else {
-			SDL_BlitSurface(image, NULL, window_surface, NULL);
-			SDL_UpdateWindowSurface(window);
-			SDL_Delay(2000);
-		}
+		return -1;
 	}
+
+	if (window == NULL) {
+		printf("ERROR: window is null!\n");
+	}
+	if (surface == NULL) {
+		printf("ERROR: surface is null!\n");
+	}
+
+	if (!load_bitmap(&image, bitmap)) {
+		printf("Failed to load the image!\n");
+		if (image != NULL) {
+			SDL_FreeSurface(image);
+		}
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
+
+	SDL_BlitSurface(image, NULL, surface, NULL);
+	SDL_Delay(500);
+	SDL_UpdateWindowSurface(window);
+
+	SDL_Delay(2000);
 
 	SDL_FreeSurface(image);
 	SDL_DestroyWindow(window);
--- src/own/lesson-02/sdladdon.c
+++ src/own/lesson-02/sdladdon.c
@@ -5,37 +5,41 @@
 #include "error.h"
 #include "settings.h"
 
-bool init(SDL_Window *window, SDL_Surface *surface, char *title)
+bool init(SDL_Window **window, SDL_Surface **surface, char *title)
 {
 	bool success = true;
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		print_sdl_error();
+		success = false;
+	}
+
+	*window = SDL_CreateWindow(title,
+				  SDL_WINDOWPOS_UNDEFINED,
+				  SDL_WINDOWPOS_UNDEFINED,
+				  640,
+				  480,
+				  SDL_WINDOW_SHOWN);
+	if (*window == NULL) {
 		print_sdl_error();
 		success = false;
 	} else {
-		window = SDL_CreateWindow(title,
-					  SDL_WINDOWPOS_UNDEFINED,
-					  SDL_WINDOWPOS_UNDEFINED,
-					  640,
-					  480,
-					  SDL_WINDOW_SHOWN);
-		if (window == NULL) {
+		*surface = SDL_GetWindowSurface(*window);
+		if (*surface == NULL) {
 			print_sdl_error();
 			success = false;
-		} else {
-			surface = SDL_GetWindowSurface(window);
 		}
 	}
 
 	return success;
 }
 
-bool load_bitmap(SDL_Surface *surface, char *filename)
+bool load_bitmap(SDL_Surface **surface, char *filename)
 {
 	bool success = true;
 
-	surface = SDL_LoadBMP(filename);
-	if (surface == NULL) {
+	*surface = SDL_LoadBMP(filename);
+	if (*surface == NULL) {
 		print_sdl_error();
 		success = false;
 	}
